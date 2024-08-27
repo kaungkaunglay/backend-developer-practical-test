6374727976c7836019cc9519de14589ca5e15161
AungKhantZin <viennghink@outlook.com>
2024-08-28 00:27:14 +0630

    Added Message Function in LoginRequest

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'email'    => ['required', 'email', 'min:6'],
+            'password' => ['required', 'string', 'min:6'],
+        ];
+    }
+    // give the message to user 
+    public function messages()
+    {
+        return [
+            'username.required' => 'Username is required',
+            'username.min' => 'Username must be at least 3 characters long',
+            'password.required' => 'Password is required',
+            'password.min' => 'Password must be at least 6 characters long',
+        ];
+    }
+}
